fc27927a39909d3f01f543375fd898d9c7e49699
Joachim Korittky <joachims.pda@hotmail.de>
2021-04-10 22:08:59 +0200

    Am Anfang etwas Zufall hinzufuegen.

--- src/main/java/de/korittky/viergewinnt/KI.java
+++ src/main/java/de/korittky/viergewinnt/KI.java
@@ -3,23 +3,42 @@ package de.korittky.viergewinnt;
 import de.korittky.viergewinnt.model.Brett;
 import de.korittky.viergewinnt.model.Farbe;
 
+import java.util.ArrayList;
+
+/**
+ * http://localhost:8080/replay/3,4,4,0,3,3,4,4,3,1,5,5,5,2,2,1,5,1,1,2,6,0,0,2,3,2,2,5,6,3,6,5
+ * http://localhost:8080/replay/3,2,2,6,1,6,3,6,6,1,3,3,2,2,4,3,2,2,1,1,4,4,4
+ * http://localhost:8080/replay/3,2,2,6,1,6,3,6,6,1,3,3,2,2,1,1,4,2,4,4,4
+ * http://localhost:8080/replay/3,2,2,6,1,6,3,6,6,1,3,3,2,2,4,3,4,3,4,4,1
+ */
+
+
 public class KI {
-    private final static int MAX_TIEFE = 9;
+    private final static int MAX_TIEFE = 8;
     private final static double scores[] = {134217728,16777216,2097152,262144,32768,4096,512,64,8,1};
 
     public static int besterZug(Brett brett) {
         int j = 0;
-        double best = -100000000;
+        double best = -200000000;
+        ArrayList<Integer> bestSlots = new ArrayList<>(Brett.BRETT_BREITE);
         for (int i=0; i<Brett.BRETT_BREITE; i++) {
             double guete = comuterZug(brett, i, brett.getFarbe(), 0);
 
             brett.score[i]=guete;
-            if (guete > best && brett.slotFrei(i)) {
-                best = guete;
-                j = i;
+            if (brett.slotFrei(i)) {
+                if (guete > best) {
+                    bestSlots.clear();
+                    bestSlots.add(i);
+                    best = guete;
+                } else if (guete == best) {
+                    // gleich gut
+                    bestSlots.add(i);
+                }
             }
         }
-        return j;
+
+        // zufällig unter den besten Zügen einen aussuchen
+        return bestSlots.get((int)(java.lang.Math.random() * bestSlots.size()));
     }
 
     private static double comuterZug(Brett brett, int slot, Farbe farbe, int tiefe) {
